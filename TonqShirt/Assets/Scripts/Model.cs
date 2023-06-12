8753049d258199dc40ea9760aea3bee1999a1776
Atsuhisa Nakane <tontonkyu@gmail.com>
2023-06-13 00:54:00 +0900

    baseline: button to change model

---
+++ TonqShirt/Assets/Scripts/Model.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Model : MonoBehaviour
+{
+    [SerializeField] private Sprite[] _sprites;
+
+    private Image _img;
+
+    void Start()
+    {
+        _img = GetComponent<Image>();
+        UpdateImage(0);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void UpdateImage(int imgId)
+    {
+        _img.sprite = _sprites[imgId];
+        _img.preserveAspect = true;
+        Debug.Log(imgId);
+    }
+}
---
+++ TonqShirt/Assets/Scripts/ModelSelectButton.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelSelectButton : MonoBehaviour
+{
+    [SerializeField] private int _imgId;
+    [SerializeField] private Model _model;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        _model.UpdateImage(_imgId);
+        Debug.Log("push");
+    }
+}
